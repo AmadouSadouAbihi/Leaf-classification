b8e6620c18ecf8bf185293074e39f54dab648c97
= <=>
2020-03-28 13:11:29 -0400

    structure of the metric code

---
+++ metrics/metric.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import roc_curve
+from sklearn.metrics import roc_auc_score
+from sklearn import metrics
+import warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+
+class Metrics:
+
+    def accuracy(self, model, y, x=None, label=None, pred=None):
+        # TO DO 
+        return 0;
+
+    def confusion_matrix(self, model, y, x=None, label=None, pred=None):
+        # TO DO 
+        return 0;
+
+    def plot_roc(self, model, y, x=None, label=None, prob=None):
+        # TO DO 
+        return 0;
