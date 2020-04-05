a7664e1e9dc91af1baac8f0aea8c77c3c50c13a6
AmadouSadouAbihi <amadouba2105@gmail.com>
2020-04-05 18:19:23 -0400

    adding the code of the metrics function

--- metrics/metric.py
+++ metrics/metric.py
@@ -1,7 +1,5 @@
 import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix
-from sklearn.metrics import roc_curve
-from sklearn.metrics import roc_auc_score
 from sklearn import metrics
 import warnings
 warnings.filterwarnings("ignore", category=DeprecationWarning)
@@ -9,14 +7,39 @@ warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 class Metrics:
 
-    def accuracy(self, model, y, x=None, label=None, pred=None):
-        # TO DO 
-        return 0;
+    def accuracy(self, model, t, x=None, label=None, pred=None):
+    	"""
+    	   label : Training | testing
+    	"""
+        if pred is None:
+            print(label + ' accuracy', round(model.score(x, t) * 100, 2), " %")
+        else:
+            print('Testing accuracy after cross-validation ', round(metrics.accuracy_score(pred,t) * 100, 2), " %")
+
 
     def confusion_matrix(self, model, y, x=None, label=None, pred=None):
-        # TO DO 
-        return 0;
+       if pred is None:
+            plt.imshow(confusion_matrix(y, model.predict(x)), interpolation='nearest', cmap=plt.cm.Blues)
 
-    def plot_roc(self, model, y, x=None, label=None, prob=None):
-        # TO DO 
-        return 0;
+            for i in range(confusion_matrix(y, model.predict(x)).shape[0]):
+                for j in range(confusion_matrix(y, model.predict(x)).shape[1]):
+                    plt.text(j, i, str([['TN', 'FP'], ['FN', 'TP']][i][j]) + " = " + str(confusion_matrix(y, model.predict(x))[i, j]), ha="center", va="center")
+
+            plt.title("CONFUSION MATRIX VISUALIZATION OF THE "+label.upper())
+
+        else:
+            plt.imshow(confusion_matrix(y, pred), interpolation='nearest', cmap=plt.cm.Blues)
+
+            for i in range(confusion_matrix(y, pred).shape[0]):
+                for j in range(confusion_matrix(y, pred).shape[1]):
+                    plt.text(j, i, str([['TN', 'FP'], ['FN', 'TP']][i][j]) + " = " + str(confusion_matrix(y, pred)[i, j]), ha="center", va="center")
+
+            plt.title("CONFUSION MATRIX VISUALIZATION AFTER VALIDATION ")
+
+        plt.ylabel("True Values")
+        plt.xlabel("Predicted Values")
+        plt.xticks([])
+        plt.yticks([])
+        plt.show()
+
+    
