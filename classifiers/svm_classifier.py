54dbb5c31515f0faa335ac413be80859a1c23ddd
AmadouSadouAbihi <amadouba2105@gmail.com>
2020-04-07 11:49:21 -0400

    Adding the SVM classifier

---
+++ classifiers/svm_classifier.py
@@ -0,0 +1,10 @@
+import os, sys
+sys.path.append(os.path.dirname(os.path.join(os.getcwd())))
+from sklearn.svm import SVC
+from classifiers.abstract_classifier import AbstractClassifier
+
+
+class SVMClassifier(AbstractClassifier):
+
+    def __init__(self, kernel='rbf', C=1, gamma=1, approch='0'):
+        super().__init__(SVC(probability=True, kernel=kernel, gamma=gamma, C=C), approch)
