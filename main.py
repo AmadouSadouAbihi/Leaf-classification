11d4ba5325defee0051558e52fcce47b58ab774f
AmadouSadouAbihi <amadouba2105@gmail.com>
2020-04-07 18:27:45 -0400

    Adding the main structure of the code

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print 'hello world'
+import os, sys
+sys.path.append(os.path.dirname(os.path.join(os.getcwd())))
+import numpy as np
+
+from classifiers.logistic_regression import LogisticRegressionClassifier
+from classifiers.random_forest import RandomForestAlgorithmClassifier
+from classifiers.svm_classifier import SVMClassifier
+
+def main():
+
+	return 0;
+
+if __name__ == "__main__":
+    main()
