9c88d14b6da7ff508b8010a2fdebe420a175405b
AmadouSadouAbihi <amadouba2105@gmail.com>
2020-04-10 19:54:38 -0400

    cross validation

--- cross_validation/cross_validation.py
+++ cross_validation/cross_validation.py
@@ -1,6 +1,6 @@
 import os, sys
 from sklearn.model_selection import StratifiedKFold, GridSearchCV
-from metrics.metrics import Metrics
+from metrics.metric import Metrics
 sys.path.append(os.path.dirname(os.path.join(os.getcwd())))
 
 
@@ -8,14 +8,24 @@ class CrossValidation:
     """
     This class does k cross validation
     """
-    def __init__(self, model, hyperparameters, kfold):
 
-    	return 0;
+    def __init__(self, model, hyperparameters, kfold):
+        self.metrics = Metrics()
+        cross_validation = StratifiedKFold(n_splits=kfold, shuffle=True)
+        self.clf = GridSearchCV(model, hyperparameters, cv=cross_validation, n_jobs=-1, verbose=1)
 
     def fit_and_predict(self, x_train, t_train, x_test, t_test, metrics):
+        prediction = self.clf.fit(x_train, t_train).best_estimator_.predict(x_test)
 
-    	return 0;
+        if metrics == "accuracy":
+            self.metrics.accuracy(self.clf, y=t_test, pred=prediction)
 
-    def get_score(self, x_test, y_test):
+        elif metrics == "confusion_matrix":
+            self.metrics.confusion_matrix(self.clf, y=t_test, pred=prediction)
 
-    	return 0;
+        elif metrics == "roc":
+            prob = self.clf.fit(x_train, t_train).best_estimator_.predict_proba(x_test)
+            self.metrics.plot_roc(self.clf, y=t_test, prob=prob[:, 1])
+
+    def get_score(self, x_test, t_test):
+        return round(self.clf.score(x_test, t_test) * 100, 2)
