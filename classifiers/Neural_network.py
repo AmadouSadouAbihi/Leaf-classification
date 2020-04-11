e0c2e2dc7c38f046f13d0e5c5639f0c682ee0133
AmadouSadouAbihi <amadouba2105@gmail.com>
2020-04-10 20:26:21 -0400

    Bug fix 2

--- classifiers/Neural_network.py
+++ classifiers/Neural_network.py
@@ -6,7 +6,7 @@ from classifiers.abstract_classifier import AbstractClassifier
 
 
 
-class Neuralnetwork(AbstractClassifier):
+class NeuralnetworkClassifier(AbstractClassifier):
     def __init__(self, hidden_layer_sizes=(10,), activation='relu', alpha=1e-2, learning_rate_init=1e-3, solver='adam', approch='0'):
         self.model = MLPClassifier(max_iter=1000,random_state=None,hidden_layer_sizes=hidden_layer_sizes,activation=activation,alpha=alpha, learning_rate_init=learning_rate_init,solver=solver)
         super().__init__(self.model, approch)
